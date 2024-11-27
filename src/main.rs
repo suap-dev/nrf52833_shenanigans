14319487a321d2b234f92cb8f502e4dbe65e01f2
suap-dev <suap.dev@gmail.com>
2024-11-27 23:46:46 +0100

    sh1106 oled display is operational

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,103 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::OutputPin;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_10X20, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use embedded_hal::spi::MODE_0;
 use nrf52833_hal::{
-    gpio::{Disconnected, Level, Output, Pin, PushPull},
+    gpio::{Disconnected, Level, Pin},
     pac::Peripherals,
-    Timer,
+    spi::{Frequency, Pins, Spi},
 };
 use panic_halt as _;
+use sh1106::{mode::GraphicsMode, Builder};
+
+const PUBLIC_SERVICE_ANNOUNCEMENT: &str =
+    "This is another public service announcement brought to you, in part, by Slim Shady.";
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let mut timer = Timer::new(peripherals.TIMER0);
+    let pins = Pins {
+        sck: unsafe {
+            Some(
+                Pin::<Disconnected>::from_psel_bits(to_hal(0).unwrap())
+                    .into_push_pull_output(Level::Low),
+            )
+        },
+        mosi: unsafe {
+            Some(
+                Pin::<Disconnected>::from_psel_bits(to_hal(1).unwrap())
+                    .into_push_pull_output(Level::Low),
+            )
+        },
+        miso: None,
+    };
 
-    // let pins: [Pin<Disconnected>; 49] = core::array::from_fn(|i| unsafe { Pin::from_psel_bits(i as u32) });
-    let mut pins: [Pin<Output<PushPull>>; 49] = core::array::from_fn(|i| {
-        unsafe { Pin::<Disconnected>::from_psel_bits(i as u32) }.into_push_pull_output(Level::Low)
-    });
+    let peripherals = Peripherals::take().unwrap();
+    let mut display: GraphicsMode<_> = Builder::new()
+        .connect_spi(
+            Spi::new(peripherals.SPI0, pins, Frequency::M4, MODE_0),
+            unsafe {
+                Pin::<Disconnected>::from_psel_bits(to_hal(2).unwrap())
+                    .into_push_pull_output(Level::High)
+            },
+            unsafe {
+                Pin::<Disconnected>::from_psel_bits(to_hal(3).unwrap())
+                    .into_push_pull_output(Level::High)
+            },
+        )
+        .into();
+
+    let mut _reset_pin: Pin<nrf52833_hal::gpio::Output<nrf52833_hal::gpio::PushPull>> =
+        unsafe { Pin::<Disconnected>::from_psel_bits(to_hal(4).unwrap()) }
+            .into_push_pull_output(Level::High);
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_10X20)
+        .text_color(BinaryColor::On)
+        .build();
+    let text = Text::with_baseline(
+        PUBLIC_SERVICE_ANNOUNCEMENT,
+        Point::zero(),
+        text_style,
+        Baseline::Middle,
+    );
+    let mut translation = Point::new(128, 32);
+
+    display.init().unwrap();
 
     #[allow(unused_must_use)]
     loop {
-        for pin in (0..=21).filter_map(|pin| to_hal(pin).ok()) {
-            pins[pin].set_high();
-            timer.delay(125_000);
-            pins[pin].set_low();
-        }
+        display.clear();
+        text.translate(translation).draw(&mut display).unwrap();
+        display.flush().unwrap();
+
+        wrapping_decr(&mut translation.x, 1, PUBLIC_SERVICE_ANNOUNCEMENT.len(), 10);
+    }
+}
+
+// this stinks a little bit
+fn wrapping_decr(num: &mut i32, decr_value: i32, messg_len: usize, font_width: i32) {
+    *num -= decr_value;
+    if *num < -(messg_len as i32 * font_width) {
+        *num = 128;
     }
 }
 
 #[derive(Debug)]
 struct InvalidMicrobitPinError;
 
-fn to_hal(microbit_pin: usize) -> Result<usize, InvalidMicrobitPinError> {
+fn to_hal(microbit_pin: u32) -> Result<u32, InvalidMicrobitPinError> {
     const HAL_ADDRESSESS: [i32; 21] = [
         2, 3, 4, 31, 28, 14, 37, 11, 10, 9, 30, 23, 12, 17, 1, 13, 34, -1, -1, 26, 32,
     ];
 
     HAL_ADDRESSESS
-        .get(microbit_pin)
+        .get(microbit_pin as usize)
         .copied()
         .ok_or(InvalidMicrobitPinError)?
         .try_into()
@@ -70,13 +129,13 @@ fn to_hal(microbit_pin: usize) -> Result<usize, InvalidMicrobitPinError> {
     23 -> 11
     24 -> ROW_3
     25 -> ?
-    26 -> 19
+    26 -> 19 / I2C_SCL
     27 -> ?
     28 -> 4 / COL_0
     29 -> ?
     30 -> 10 / COL_4
     31 -> 3 / COL_2
-    32 -> 20
+    32 -> 20 / I2C_SDA
     33 -> ?
     34 -> 16
     35 -> ?
@@ -87,6 +146,11 @@ fn to_hal(microbit_pin: usize) -> Result<usize, InvalidMicrobitPinError> {
 // const FIRST_PIN: u32 = 0;
 // const LAST_PIN: u32 = 48;
 // const SPEAKER: [usize; 1] = [0];
+// const I2C: [usize; 2] = [26, 32];
+// const I2C_SCL = 26;
+// const I2C_SDA = 32;
+// const MB_I2C_SCL: usize = 19;
+// const MB_I2C_SDA: usize = 20;
 // const GPIO: [usize; 19] = [
 //     2, 3, 4, 31, 28, 14, 37, 11, 10, 9, 30, 23, 12, 17, 1, 13, 34, 26, 32,
 // ]; // pins(0..=20).skip(17, 18)
